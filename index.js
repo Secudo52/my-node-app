4458abb260e73ca96de712f26a25d700e137e0ee
Secudo <danikdenishchik1@gmail.com>
2026-09-12 08:46:35 +0300

    Сделал реакцию сервака на ивенты и проверил как работают синхронные команды

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 const http = require('http');
+const EventEmitter = require('events');
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-  res.end('<h1>Привет, мир!</h1>');
+class AppServer extends EventEmitter {
+    constructor() {
+        super();
+        
+        this.server = http.createServer((req, res) => {
+            this.emit('request:received', { url: req.url, method: req.method });
+            
+            res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+            res.end('Hello from Event-Driven Server!');
+        });
+    }
+
+    start(port) {
+        this.server.listen(port, () => {
+            this.emit('server:started', port);
+        });
+    }
+
+    stop() {
+        this.server.close(() => {
+            this.emit('server:stopped');
+        });
+    }
+}
+
+const app = new AppServer();
+
+app.on('server:started', (port) => {
+    console.log(`Server zapyshchen na porty ${port}`);
 });
 
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`Сервер запущен на http://localhost:${PORT}`);
+app.on('request:received', (requestData) => {
+    console.log(`Polychen zapros: ${requestData.method} ${requestData.url}`);
 });
+
+app.on('server:stopped', () => {
+    console.log(`Servak sotanovlen`);
+});
+
+app.start(3000);
+
+setTimeout(() => {
+    app.stop();
+}, 10000);
